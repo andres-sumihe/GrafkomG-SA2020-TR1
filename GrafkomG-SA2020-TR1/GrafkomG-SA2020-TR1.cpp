e99e8e4a93a920b88bdb514a2de7cb6ab3a797e1
andres-sumihe <672018136@student.uksw.edu>
2020-07-18 14:58:59 +0700

    Add some header

--- GrafkomG-SA2020-TR1/GrafkomG-SA2020-TR1.cpp
+++ GrafkomG-SA2020-TR1/GrafkomG-SA2020-TR1.cpp
@@ -1,39 +1,8 @@
 #include <iostream>
 #include <GL/freeglut.h>
+#include "Controller.h"
 
-int is_depth;
-
-int main(int argc, char** argv){
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-	glutInitWindowSize(1920, 1080);
-	glutInitWindowPosition(0, 0);
-	glutCreateWindow("Taman Anggrek Mall - 3D");
-	init();
-	glutDisplayFunc(tampil);
-	glutReshapeFunc(ukuran);
-	glutMainLoop();
-	return 0;
-}
-
-void init(void) {
-	glClearColor(224 / 255, 255 / 255, 255 / 255, 0.0);
-	glMatrixMode(GL_PROJECTION);
-	glEnable(GL_DEPTH_TEST);
-	is_depth = 1;
-	glMatrixMode(GL_MODELVIEW);
-	glPointSize(9.0);
-	glLineWidth(1.0f);
-}
-
-void tampil(void) {
-	if (is_depth)
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	else
-		glClear(GL_COLOR_BUFFER_BIT);
-	
-	glutSwapBuffers();
-}
+controller cc;
 
 void ukuran(int lebar, int tinggi) {
 	if (tinggi == 0) tinggi = 1;
@@ -44,3 +13,26 @@ void ukuran(int lebar, int tinggi) {
 	glTranslatef(0.0, -5.0, -400.0);
 	glMatrixMode(GL_MODELVIEW);
 }
+
+void tampil(void) {
+	if (is_depth)
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	else
+		glClear(GL_COLOR_BUFFER_BIT);
+
+	glutSwapBuffers();
+}
+
+int main(int argc, char** argv){
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(1920, 1080);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Taman Anggrek Mall - 3D");
+	cc.init();
+	glutDisplayFunc(tampil);
+	glutReshapeFunc(ukuran);
+	glutMainLoop();
+	return 0;
+}
+
---
+++ GrafkomG-SA2020-TR1/Mall.h
@@ -0,0 +1,12 @@
+#include <GL/freeglut.h>
+// Author: Ado Nugroho
+class Mall {
+public:
+	void dinding(int z1, int z2) {
+
+	}
+
+	void garis_dinding(int z1, int z2) {
+
+	}
+};
